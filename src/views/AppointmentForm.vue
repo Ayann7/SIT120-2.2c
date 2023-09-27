26e28a1937a70b9b1096c5d76c6be0c9cd373916
Ayann7 <129643878+Ayann7@users.noreply.github.com>
2023-09-27 19:36:25 +1000

    Add files via upload

---
+++ src/views/AppointmentForm.vue
@@ -0,0 +1,66 @@
+<template>
+    <form class="appointment-form">
+      <h2>Book an Appointment with Us</h2>
+      <img src="carappointment.jpeg" alt="Appointment Image" class="appointment-image" />
+      <div class="form-group">
+        <label for="name">Name:</label>
+        <input type="text" id="name" v-model="name" required />
+      </div>
+      <div class="form-group">
+        <label for="phone">Phone:</label>
+        <input type="tel" id="phone" v-model="phone" required />
+      </div>
+      <div class="form-group">
+        <label for="appointment-time">Appointment Time:</label>
+        <input type="datetime-local" id="appointment-time" v-model="appointmentTime" required />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        name: '',
+        phone: '',
+        appointmentTime: '',
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .appointment-form {
+    text-align: center;
+    padding: 20px;
+  }
+  
+  h2 {
+    font-size: 24px;
+  }
+  
+  .appointment-image {
+    max-width: 100%;
+    height: auto;
+    margin: 20px 0;
+  }
+  
+  .form-group {
+    margin: 10px 0;
+  }
+  
+  label {
+    font-weight: bold;
+  }
+  
+  input[type="text"],
+  input[type="tel"],
+  input[type="datetime-local"] {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  }
+  </style>
+  
---
+++ src/views/Home.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="home">
+    <div class="header">
+      <h1>Welcome to Cheap Car Rentals</h1>
+      <p>Your trusted partner for affordable and reliable car rentals.</p>
+      <img src="contact.jpeg" alt="Cheap Car Rentals Logo" height="400" width="600" />
+    </div>
+    <nav>
+      <router-link to="/rent">Rent a Car</router-link>
+      <router-link to="/appointment">Book an Appointment</router-link>
+      <router-link to="/contact">Contact Us</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+.header {
+  background-color: #007BFF;
+  color: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+}
+
+.logo {
+  max-width: 100px;
+  margin-bottom: 20px;
+}
+
+h1 {
+  font-size: 36px;
+  margin: 0;
+  padding-top: 10px;
+}
+
+nav {
+  margin-top: 20px;
+}
+
+nav a {
+  margin: 0 10px;
+  text-decoration: none;
+  font-size: 18px;
+  color: #007BFF;
+  transition: color 0.3s;
+}
+
+nav a:hover {
+  color: #0056b3;
+}
+.header p {
+  font-size: 18px;
+  margin-top: 10px;
+  color: #fff;
+}
+</style>
